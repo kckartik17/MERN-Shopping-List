2ff6e0d6a143d0000350b2071a32e635ee49d243
Kartik Chauhan <kckartik17@gmail.com>
2019-02-12 18:31:34 +0530

    deploying
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -24,6 +25,14 @@ mongoose
 
 //Use routes
 app.use("/api/items", items);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
